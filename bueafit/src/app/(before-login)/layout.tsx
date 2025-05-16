cf95b73abd89d050ac5ec81fe8e4561a98e18950
Sion <solee9802@gmail.com>
2025-05-17 01:18:46 +0900

    로그인이 돼 있으면 로그인, 회원가입 페이지, 소개 페이지 진입 불가 설정

--- bueafit/src/app/(before-login)/layout.tsx
+++ bueafit/src/app/(before-login)/layout.tsx
@@ -1,11 +1,19 @@
 import { Suspense } from "react";
 import LoginNav from "../components/LoginNav";
+import { redirect } from "next/navigation";
+import { cookies } from "next/headers";
 
-export default function Layout({
+export default async function Layout({
     children
 }: Readonly<{
     children: React.ReactNode
 }>) {
+    // 액세스 토큰 or 리프레시 토큰이 있을 때 로그인, 회원가입 페이지 못오게
+    const refresh_token = (await (cookies())).get('refresh_token')
+    if(refresh_token) {
+        redirect('/store/main');
+    }
+    
     return (
         <div className="min-h-screen">
             <Suspense>
--- bueafit/src/app/page.tsx
+++ bueafit/src/app/page.tsx
@@ -1,6 +1,14 @@
+import { redirect } from "next/navigation";
 import LoginNav from "./components/LoginNav";
+import { cookies } from "next/headers";
 
-export default function Home() {
+export default async function Home() {
+  // 액세스 토큰 or 리프레시 토큰이 있을 때 로그인, 소개, 회원가입 페이지 못오게
+  const refresh_token = (await (cookies())).get('refresh_token')
+  if(refresh_token) {
+      redirect('/store/main');
+  }
+  
   return (
     <div>
       <LoginNav />
