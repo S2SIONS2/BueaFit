776472e02bc4703771aede910a76455297117e8c
Sion <solee9802@gmail.com>
2025-05-26 22:55:59 +0900

    메인 페이지 차트 외 데이터 연결

--- bueafit/src/app/(after-login)/store/main/page.tsx
+++ bueafit/src/app/(after-login)/store/main/page.tsx
@@ -5,18 +5,31 @@ import { faCheckToSlot, faClipboardList } from "@fortawesome/free-solid-svg-icon
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useEffect, useState } from "react";
 import dayjs from "dayjs";
+import utc from "dayjs/plugin/utc";
 import LoadingSpinner from "@/app/components/LoadingSpinner";
 
+type Treatment = {
+        status: string;
+        reserved_at: string;
+        [key: string]: any;
+};
+
 export default function Page() {
+    dayjs.extend(utc);
+    
     const date = new Date()
     const today = dayjs(date).format("YYYY-MM-DD")
+    const todayUTC = dayjs(date).utc().format("YYYY-MM-DD");
     
-    const [loading, setLoading] = useState(true)
-    const [todayStatus, setTodayStatus] = useState([]);
+    const [loading, setLoading] = useState(true) // 로딩 상태
+
+    const borderColors = ["border-blue-400", "border-green-400", "border-pink-400"]; // 예약 표기 시 1/3 컬러 돌아가며 주입
+    
+    const [todayStatus, setTodayStatus] = useState<Treatment[]>([]); // 오늘의 예약 상태
     // 아직 api가 따로 없어서 프론트단에서 처리
     const fetchTreatments = async () => {
         try {
-            const res = await fetchInterceptors(`${process.env.NEXT_PUBLIC_BUEAFIT_API}/treatments?start_date=${today}&end_date=${today}`, {
+            const res = await fetchInterceptors(`${process.env.NEXT_PUBLIC_BUEAFIT_API}/treatments`, { // ?start_date=${todayUTC}&end_date=${todayUTC}
                 method: "GET",
                 headers: {
                     "Content-Type" : 'application/json'
@@ -25,7 +38,8 @@ export default function Page() {
             const data = await res.json();
 
             if(res.status === 200) {
-                setTodayStatus(data.items)
+                const filteredItems = data.items.filter((item) => item.reserved_at.slice(0,10) === todayUTC);
+                setTodayStatus(filteredItems)
             }
         }catch(e) {
             console.error(e)
@@ -54,21 +68,23 @@ export default function Page() {
         <div className="w-full p-6 bg-[#f8f9fb]">
             <header className="mb-6">
                 <h1 className="text-2xl font-bold">금일 현황</h1>
-                <p className="text-sm text-gray-500">{today}</p>
+                <p className="text-sm font-bold text-gray-500">{today}</p>
             </header>
 
             <section className="grid grid-cols-1 sm:grid-cols-3 gap-4 mb-8">
                 <div className="bg-gradient-to-r from-purple-300 to-purple-500 text-white rounded-lg p-4 shadow">
                     <h3 className="text-sm font-medium">오늘 예약 수</h3>
-                    <p className="text-3xl font-bold mt-1">8건</p>
+                    <p className="text-3xl font-bold mt-1">{todayStatus.length}건</p>
                 </div>
                 <div className="bg-gradient-to-r from-green-300 to-green-500 text-white rounded-lg p-4 shadow">
                     <h3 className="text-sm font-medium">완료된 시술</h3>
-                    <p className="text-3xl font-bold mt-1">6건</p>
+                    <p className="text-3xl font-bold mt-1">{todayStatus.filter((v) => v.status === "COMPLETED").length}건</p>
                 </div>
                 <div className="bg-gradient-to-r from-red-300 to-red-500 text-white rounded-lg p-4 shadow">
                     <h3 className="text-sm font-medium">노쇼 수</h3>
-                    <p className="text-3xl font-bold mt-1">2건</p>
+                    <p className="text-3xl font-bold mt-1">
+                        {(todayStatus.filter((v) => v.status === "NO_SHOW").length > 0) ? todayStatus.filter((v) => v.status === "NO_SHOW").length : "0"}건
+                    </p>
                 </div>
             </section>
 
@@ -79,20 +95,22 @@ export default function Page() {
                             <FontAwesomeIcon icon={faClipboardList} className="text-violet-400 mr-2" />
                             예약 내역
                         </h3>
-                        <ul className="space-y-3 text-sm">
-                            <li className="border-l-4 border-blue-400 pl-3">
-                                <p className="font-semibold">10:00 이준영</p>
-                                <p className="text-gray-500">두피 관리</p>
-                            </li>
-                            <li className="border-l-4 border-green-400 pl-3">
-                                <p className="font-semibold">11:30 김영희</p>
-                                <p className="text-gray-500">스킨 케어</p>
-                            </li>
-                            <li className="border-l-4 border-pink-400 pl-3">
-                                <p className="font-semibold">13:00 박철수</p>
-                                <p className="text-gray-500">손 마사지</p>
-                            </li>
-                        </ul>
+                        {
+                            todayStatus.map((item, index) => (
+                                <ul key={index} className="space-y-3 text-sm max-h-[200px] overflow-y-auto">
+                                    <li className={`border-l-4 ${borderColors[index % borderColors.length]} pl-2 mb-4`}>
+                                        <p className="font-semibold">{item.phonebook.name}</p>
+                                        <p className="text-gray-500">
+                                            {item.treatment_items.map((treatment, idx) => (
+                                                <span key={idx} className="inline-block mr-1">
+                                                    {treatment.menu_detail.name}
+                                                </span>
+                                            ))}
+                                        </p>
+                                    </li> 
+                                </ul>
+                            ))
+                        }
                     </div>
 
                     <div className="h-full bg-white rounded-xl p-4 shadow lg:col-span-2">
@@ -100,20 +118,31 @@ export default function Page() {
                             <FontAwesomeIcon icon={faCheckToSlot} className="text-violet-400 mr-2"/> 
                             작업 완료 내역
                         </h3>
-                        <ul className="list-disc pl-5 space-y-1 text-sm text-gray-700">
-                            <li>홍길동 - 두피 관리</li>
-                            <li>김영희 - 스킨 케어</li>
-                            <li>박철수 - 손 마사지</li>
-                        </ul>
+                        {
+                            todayStatus.filter((v) => v.status === "COMPLETED").length > 0 ? 
+                            todayStatus.filter((v) => v.status === "COMPLETED").map((item, index) => (
+                                <div key={index} className="border-b border-gray-200 py-2">
+                                    <p className="font-semibold">{item.phonebook.name}</p>
+                                    <p className="text-gray-500">
+                                        {item.treatment_items.map((treatment, idx) => (
+                                            <span key={idx} className="inline-block mr-1">
+                                                {treatment.menu_detail.name}
+                                            </span>
+                                        ))}
+                                    </p>
+                                </div>
+                            )) : 
+                            <p className="text-gray-500">오늘 완료된 작업이 없습니다.</p>   
+                        }
                     </div>
                 </div>
 
             </section>
             <section className="lg:col-span-3 space-y-6">
-                <div className="bg-white rounded-xl p-4 shadow">
+                {/* <div className="bg-white rounded-xl p-4 shadow">
                     <h3 className="text-sm font-semibold text-gray-700 mb-2">직원별 매출</h3>
                     <div className="h-40 bg-gray-100 rounded flex items-center justify-center text-gray-400">[BarChart]</div>
-                </div>
+                </div> */}
                 <div className="bg-white rounded-xl p-4 shadow">
                     <h3 className="text-sm font-semibold text-gray-700 mb-2">시술별 매출</h3>
                     <div className="h-40 bg-gray-100 rounded flex items-center justify-center text-gray-400">[PieChart]</div>
